5cbe359debb58e2a109c56da542bf778fb2768f3
HusainSuksar <hussainsuksar101@gmail.com>
2025-08-08 12:16:53 +0530

    minor changes

--- umoor_sehhat/urls.py
+++ umoor_sehhat/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, RedirectView
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
